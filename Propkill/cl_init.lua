570f08008c63b234252adfb2bd9f3701a89ab347
Gooseyg <lganose@gmail.com>
2013-05-29 23:33:37 +0200

    Added Ip Addresses

--- Propkill/cl_init.lua
+++ Propkill/cl_init.lua
@@ -1,11 +1,16 @@
-include('shared.lua') 
+ include('shared.lua') 
 
 local pl = LocalPlayer()
 
  net.Receive("PlayerWelcomeMessage", function(pl)
-   chat.AddText(Color(0,204,204), "Hello, ", Color(0,255,0), pl:Nick(), Color(0,204,204), ", welcome to the server!")
+ 	chat.AddText(Color(0,204,204), "Hello, ", Color(0,255,0), pl:Nick(), Color(0,204,204), ", welcome to the server!")
  end)
 
 net.Receive("ServerJoinMessage", function()
-	chat.AddText(Color(0,255,0), net.ReadEntity():Nick(), Color(0,204,204), " with SteamID: ", Color(0,255,0), net.ReadEntity():SteamID(), Color(0,204,204), " has joined the server.") 
+	local net.ReadEntity() = ply
+	chat.AddText(Color(0,255,0), ply:Nick(), Color(0,204,204), " with SteamID: ", Color(0,255,0), ply:SteamID(), Color(0,204,204), " has joined the server.")
+	if pl:IsAdmin() or pl:IsSuperAdmin() then
+		return end
+	else chat.AddText(Color(0,204,204),ply:Nick().."'s IP Address is: ", Color(0,255,0), ply:IPAddress())
+	end
 end)
