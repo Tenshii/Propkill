a738f540255ee6ec19ea4341e81dfda8d04778c8
Gooseyg <lganose@gmail.com>
2013-05-29 22:50:26 +0200

    Update init.lua

--- Propkill/init.lua
+++ Propkill/init.lua
@@ -1,5 +1,56 @@
 /*---------------------------------------------------------
    Author: .Tenshi (STEAM_0:1:47827646) and Goose (STEAM_0:0:10650470)
 ---------------------------------------------------------*/
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+include( 'shared.lua' )
 
-DeriveGamemode( "sandbox" )
+// Setting the Loadouts and welcome messages:
+-- Starting in spectator
+
+util.AddNetworkString( "PlayerWelcomeMessage" )
+util.AddNetworkString( "ServerJoinMessage" )
+
+function GM:PlayerInitialSpawn( pl )
+	pl:SetTeam( 3 )
+	net.Start( "PlayerWelcomeMessage" )
+	net.Send( pl )
+	net.Start( "ServerJoinMessage" )
+	net.String( pl:Nick() )
+	net.Broadcast()
+	
+end
+
+// Weapons which teams spawn with:
+-- 1&2 only get physguns and 3 gets nothing
+
+function GM:PlayerLoadOut( pl )
+   if pl:Team() == 1 then
+      pl:Give("weapon_physgun")
+      pl:Model("models/breen.mdl")
+      pl:SetColor(255,0,0,255)
+   elseif pl:Team() == 2 then
+      pl:Give("weapon_physgun")
+      pl:Model("models/gman.mdl")
+      pl:SetColor(0,0,255,255)
+   elseif pl:Team() == 3 then
+   	return nil
+   end
+end
+
+// What happens on spawn:
+-- Spawns team 3 as spectator
+
+function GM:PlayerSpawn( pl )
+   if pl:Team() == 3 then
+      GAMEMODE:PlayerSpawnAsSpectator( pl )
+   end
+end
+
+// Instantly respawns player on death
+
+function GM:PlayerDeathThink( pl )
+   pl:Spawn()
+end
+
+   
