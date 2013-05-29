0cc4150844b7aeea5e1b7d2bb9a6c924b692ba52
Gooseyg <lganose@gmail.com>
2013-05-29 22:33:07 +0200

    shared.lua file set up

---
+++ Propkill/shared.lua
@@ -0,0 +1,6 @@
+GM.Name  	= "PropKill"
+GM.Author	= ".Tenshi (STEAM_0:1:47827646) and Goose (STEAM_0:0:10650470)"
+ 
+team.SetUp( 1, "Reds", Color( 255, 0, 0, 255 ) ) -- Red Team (Team 1)
+team.SetUp( 2, "Blues", Color( 0, 0, 255, 255 ) ) -- Blue Team (Team 2)
+team.SetUp( 3, "Spectator", Color( 156, 156, 156, 255 ) ) -- Spectators (Team 3)
