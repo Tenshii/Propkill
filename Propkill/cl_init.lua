e67d5a39f0d95be51a4ca62765f9c76ba3e129c6
Gooseyg <lganose@gmail.com>
2013-05-30 01:15:11 +0200

    fukdis

--- Propkill/cl_init.lua
+++ Propkill/cl_init.lua
@@ -1,16 +1,2 @@
- include('shared.lua') 
-
-local pl = LocalPlayer()
-
- net.Receive("PlayerWelcomeMessage", function(pl)
- 	chat.AddText(Color(0,204,204), "Hello, ", Color(0,255,0), pl:Nick(), Color(0,204,204), ", welcome to the server!")
- end)
-
-net.Receive("ServerJoinMessage", function()
-	local net.ReadEntity() = ply
-	chat.AddText(Color(0,255,0), ply:Nick(), Color(0,204,204), " with SteamID: ", Color(0,255,0), ply:SteamID(), Color(0,204,204), " has joined the server.")
-	if pl:IsAdmin() or pl:IsSuperAdmin() then
-		return end
-	else chat.AddText(Color(0,204,204),ply:Nick().."'s IP Address is: ", Color(0,255,0), ply:IPAddress())
-	end
-end)
+include('shared.lua') 
+include('cl_hud.lua')
