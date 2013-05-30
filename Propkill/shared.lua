191d668f933def640f81f39db04662ef5b494739
Gooseyg <lganose@gmail.com>
2013-05-30 14:34:04 +0200

    updated

--- Propkill/shared.lua
+++ Propkill/shared.lua
@@ -1,15 +1,21 @@
-GM.Name  	= "PropKill"
-GM.Author	= ".Tenshi (STEAM_0:1:47827646) and Goose (STEAM_0:0:10650470)"
+GM.Name 	= "PropKill"
+GM.Author 	= "Tenshi and Goose"
+GM.Email 	= "N/A"
+GM.Website 	= "N/A"
 
 function GM:Initialize()
 
- self.BaseClass.Initialize( self )
+	self.BaseClass.Initialize( self )
 	
 end
- 
-team.SetUp( 1, "Reds", Color( 255, 0, 0, 255 ) ) -- Red Team (Team 1)
-team.SetUp( 2, "Blues", Color( 0, 0, 255, 255 ) ) -- Blue Team (Team 2)
-team.SetUp( 3, "Spectator", Color( 156, 156, 156, 255 ) ) -- Spectators (Team 3)
 
-util.PrecacheModel( "models/breen.mdl" )
-util.PrecacheModel( "models/gman.mdl" )
+// Teams
+
+team.SetUp( 1, "Blues", Color( 0, 0, 255 ) )
+team.SetUp( 2, "Reds", Color( 255, 0, 0 ) )
+team.SetUp( 3, "Spectators", Color( 186, 186, 186 ) )
+
+// Models
+
+util.PrecacheModel( "models/player/ct_gsg9.mdl" )
+util.PrecacheModel( "models/player/t_leet.mdl" )
