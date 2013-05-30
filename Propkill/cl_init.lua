5b5d8535e87da0bb24ed179f14bc9115707dc4c1
Gooseyg <lganose@gmail.com>
2013-05-30 14:33:24 +0200

    better version

--- Propkill/cl_init.lua
+++ Propkill/cl_init.lua
@@ -1,2 +1,62 @@
-include('shared.lua') 
-include('cl_hud.lua')
+AddCSLuaFile( "cl_hud" )
+include( 'shared.lua' )
+include( 'cl_hud.lua')
+
+//teamchooser
+
+function teamchooser() 
+
+  local teamchooser = vgui.Create( "DFrame" ) -- Creates the frame itself
+	teamchooser:SetPos( 500,300 ) -- Position on the players screen
+	teamchooser:SetSize( 300, 150 ) -- Size of the frame
+	teamchooser:SetTitle( "Choose Your Team!" ) -- Title of the frame
+	teamchooser:SetVisible( true )
+	teamchooser:SetDraggable( true ) -- Draggable by mouse?
+	teamchooser:ShowCloseButton( true ) -- Show the close button?
+	teamchooser:MakePopup() -- Show the frame
+
+		 local TButton = vgui.Create( "DButton", teamchooser )
+		 TButton:SetText( "Blues" )
+		 TButton:SetPos( 35,55 )
+		 TButton:SetSize( 100, 75 )
+		 function TButton:OnMousePressed()
+		 	RunConsoleCommand("team_blue")
+		 end
+		 function TButton:OnMouseReleased()
+			teamchooser:SetVisible(false)
+		 end
+
+		 local CTButton = vgui.Create( "DButton", teamchooser )
+		 CTButton:SetText( "Reds" )
+		 CTButton:SetPos( 160,55 )
+		 CTButton:SetSize( 100,75 )
+		 function CTButton:OnMousePressed()
+		 	RunConsoleCommand("team_red")
+		 end 	
+		 function CTButton:OnMouseReleased()
+			teamchooser:SetVisible(false)
+		 end
+end
+
+
+usermessage.Hook( "teamchooser", teamchooser )
+concommand.Add( "team_menu", teamchooser )
+
+net.Receive("JoinRed", function()
+	notification.AddLegacy( net.ReadEntity():Nick().." has joined the Red team.", NOTIFY_UNDO, 2 )
+end)
+net.Receive("JoinBlue", function()
+	notification.AddLegacy( net.ReadEntity():Nick().." has joined the Blue team.", NOTIFY_UNDO, 2 )
+end)
+net.Receive("JoinSpec", function()
+	notification.AddLegacy( net.ReadEntity():Nick().." has moved to Spectate.", NOTIFY_UNDO, 2 )
+end)
+net.Receive("RedDisallow", function()
+	notification.AddLegacy( "You already are in the Red team.", NOTIFY_ERROR, 2 )
+end)
+net.Receive("BlueDisallow", function()
+	notification.AddLegacy( "You already are in the Blue team.", NOTIFY_ERROR, 2 )
+end)
+net.Receive("SpecDisallow", function()
+	notification.AddLegacy( " You already are in Spectate.", NOTIFY_ERROR, 2 )
+end)
