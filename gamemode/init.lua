6835a05a2540ccb1d4774e0b0159f9163f239688
Gooseyg <lganose@gmail.com>
2013-06-01 01:01:21 +0200

    fix

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -8,19 +8,23 @@ Coded by .Tenshi (STEAM_0:1:47827646) and Goose (STEAM_0:0:10650470)
 /==================================================/
 ]] )
 
+AddCSLuaFile( "shared.lua" )
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "cl_hud.lua" )
-AddCSLuaFile( "shared.lua" )
 AddCSLuaFile( "cl_netmessages.lua" )
 AddCSLuaFile( "cl_vgui.lua" )
 AddCSLuaFile( "sv_gmfunctions.lua" )
 AddCSLuaFile( "sv_commands.lua" )
+AddCSLuaFile( "sv_database.lua" )
+
+
 include( 'shared.lua' )
 include( 'cl_hud.lua')
 include( 'cl_netmessages.lua' )
 include( 'cl_vgui.lua' )
 include( 'sv_gmfunctions.lua' )
 include( 'sv_commands.lua' )
+include( 'sv_database.lua' )
 
 
 
