c1156bf8e5086fe6c904a80b1dbb139bd2d46a04
Gooseyg <lganose@gmail.com>
2013-06-01 16:07:01 +0200

    pls work

--- gamemode/sv_database.lua
+++ gamemode/sv_database.lua
@@ -14,9 +14,6 @@ function PK.SendInfo(pl)
 end
 
 function PK.KillStreak(pl,item,attacker)
-	if pl == attacker then
-		return nil
-	elseif pl == attacker then
 		pl.Vars.KillStreak = 0
 		attacker.Vars.KillStreak = attacker.Vars.KillStreak + 1
 
